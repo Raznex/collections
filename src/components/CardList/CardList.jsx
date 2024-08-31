c50f5e713217e5de8b41a343075b97548d795c52
AlaBeSka <ehanbegov@yandex.ru>
2024-08-31 15:58:52 +0300

    добавил pagination

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CardList.scss'; // Исправлен путь (удалил лишнее .scss)
 import productImage from '../../assets/icons/panda.jpg'; // Импорт изображения
 import Card from './../Card/Card';
+import Pagination from './../Pagination/Pagination';
 
 const CardList = ({ view }) => {
   const cardsData = [
@@ -34,23 +35,46 @@ const CardList = ({ view }) => {
     },
   ];
 
-  const cards = cardsData.map((card, index) => (
-    <Card
-      key={index}
-      id={index}
-      title={card.title}
-      manufacturer={card.manufacturer}
-      category={card.category}
-      date={card.date}
-      product={card.product}
-      description={card.description}
-      view={view}
-    />
-  ));
+  const maxCards = view === 'list' ? 8 : 21;
+  const cardsPerPage = maxCards;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastCard = currentPage * cardsPerPage;
+  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+  const currentCards = cardsData.slice(indexOfFirstCard, indexOfLastCard);
+
+  const totalPages = Math.ceil(cardsData.length / cardsPerPage);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const cards = cardsData
+    .slice(0, maxCards)
+    .map((card, index) => (
+      <Card
+        key={index}
+        id={index}
+        title={card.title}
+        manufacturer={card.manufacturer}
+        category={card.category}
+        date={card.date}
+        product={card.product}
+        description={card.description}
+        view={view}
+      />
+    ));
 
   return (
-    <div className={`cards ${view === 'list' ? 'list-view' : 'tile-view'}`}>
-      {cards}
+    <div className='cards'>
+      <div
+        className={`cards__container ${view === 'list' ? 'list-view' : 'tile-view'}`}
+      >
+        {cards}
+      </div>
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        paginate={paginate}
+      />
     </div>
   );
 };
